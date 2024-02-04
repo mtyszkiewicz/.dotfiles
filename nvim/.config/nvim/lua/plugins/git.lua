fa7d8ac5b573d88153bbe569be4f762b4ea9fc80
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2024-02-04 16:14:30 +0100

    add neovim plugin neogit

---
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",        -- Diff integration
+            "nvim-telescope/telescope.nvim",
+        },
+        config = true
+    }
+}
