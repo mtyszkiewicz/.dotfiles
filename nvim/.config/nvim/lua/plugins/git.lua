15a67aa1bf5a423253e2938f9582ab6ac9a847b2
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2024-02-05 10:23:33 +0100

    add gitsigns and diffview

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -13,5 +13,46 @@ return {
 
             vim.keymap.set("n", "<leader>pg", neogit.open)
         end
+    },
+    {
+        'lewis6991/gitsigns.nvim',
+        event = 'BufReadPre',
+        opts = {
+            -- see also: autocmds.lua
+            -- this gets toggled off in insert mode
+            -- and back on when leaving insert mode
+            current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'eol',
+                delay = 100,
+            },
+            current_line_blame_formatter = ' <abbrev_sha> | <author>, <author_time> - <summary>',
+            on_attach = function()
+                vim.cmd.redrawstatus()
+            end,
+        },
+    },
+    {
+        'sindrets/diffview.nvim',
+        cmd = {
+            'DiffviewLog',
+            'DiffviewOpen',
+            'DiffviewClose',
+            'DiffviewRefresh',
+            'DiffviewFocusFiles',
+            'DiffviewFileHistory',
+            'DiffviewToggleFiles',
+        },
+        opts = {
+            enhanced_diff_hl = true,
+            view = {
+                file_panel = {
+                    win_config = {
+                        position = 'right',
+                    },
+                },
+            },
+        },
     }
 }
