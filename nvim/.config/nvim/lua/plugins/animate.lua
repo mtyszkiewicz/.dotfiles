39ed232191d52e1db450f0f15899d82a510ef63b
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2024-02-10 01:00:53 +0100

    add neovim animate plugin

---
+++ nvim/.config/nvim/lua/plugins/animate.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        'echasnovski/mini.animate',
+        version = '*',
+        config = function()
+            local minianimate = require("mini.animate")
+
+            minianimate.setup({
+                resize = {
+                    enable = false
+                },
+                open = {
+                    enable = false
+                },
+                close = {
+                    enable = false
+                },
+                cursor = {
+                    enable = false
+                },
+                scroll = {
+                    enable = true,
+                    -- timing = minianimate.gen_timing.linear({ duration = 100, unit = 'total' }),
+                },
+            })
+        end
+    }
+}
