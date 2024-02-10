c5f8251882dfcf3659e16a57d26ec16585981147
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2024-02-10 02:04:21 +0100

    update telescope to close mini.files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,34 @@
 return {
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 
+            'nvim-lua/plenary.nvim', 
+            'echasnovski/mini.files'
+        },
         config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
-        vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+            local builtin = require('telescope.builtin')
+            local minifiles = require('mini.files')
+
+            vim.keymap.set('n', '<leader>pf', function()
+                minifiles.close()
+                builtin.find_files()
+            end, {})
+            vim.keymap.set('n', '<leader>ps', function()
+                    minifiles.close()
+                    builtin.live_grep()
+            end, {})
+            vim.keymap.set('n', '<C-p>', function()
+                    minifiles.close()
+                    builtin.git_files()
+            end, {})
+            vim.keymap.set('n', '<leader>gb', function()
+                    minifiles.close()
+                    builtin.git_branches()
+            end, {})
+            vim.keymap.set('n', '<leader>gc', function()
+                    minifiles.close()
+                    builtin.git_commits()
+            end, {})
         end,
     }
 }
