f2e133afd4f3e8d25ec9a6eb1101670fd94443c5
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2024-11-29 01:10:10 +0100

    set gruvbox dark theme

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -1,44 +1,27 @@
 return {
-    -- {
-    --     "folke/tokyonight.nvim",
-    --     lazy = true,
-    --     opts = { style = "moon" },
-    -- },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                styles = { transparency = true }
-            })
-            vim.cmd("colorscheme rose-pine-main")
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+        require('gruvbox').setup()
+        vim.cmd("colorscheme gruvbox")
+
+        local transparent_elements = {
+            "Normal",
+            "NormalNC",
+            "NormalFloat",
+            "FloatBorder",
+            "LineNr",
+            "Folded",
+            "NonText",
+            "SpecialKey",
+            "VertSplit",
+            "SignColumn",
+            "EndOfBuffer",
+            "CursorLine",
+            "WinSeparator",
+        }
+        for _, elem in pairs(transparent_elements) do
+            vim.cmd("hi " .. elem .. " guibg=NONE ctermbg=NONE")
         end
-    }
-    -- {
-    --     "Mofiqul/vscode.nvim",
-    --     config = function()
-    --         vim.o.background = 'dark'
-    --
-    --         local c = require('vscode.colors').get_colors()
-    --         require('vscode').setup({
-    --             transparent = true,
-    --             italic_comments = true,
-    --             -- Underline `@markup.link.*` variants
-    --             underline_links = true,
-    --             -- Disable nvim-tree background color
-    --             disable_nvimtree_bg = true,
-    --             -- Override colors (see ./lua/vscode/colors.lua)
-    --             -- color_overrides = {
-    --             --     vscLineNumber = '#FFFFFF',
-    --             -- },
-    --             -- Override highlight groups (see ./lua/vscode/theme.lua)
-    --             group_overrides = {
-    --                 -- this supports the same val table as vim.api.nvim_set_hl
-    --                 -- use colors from this colorscheme by requiring vscode.colors!
-    --                 Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-    --             }
-    --         })
-    --         require('vscode').load()
-    --     end
-    -- }
+    end
 }
