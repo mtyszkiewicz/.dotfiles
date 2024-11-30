3c27d35900cd115528d86328d1e43fc6120cd129
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2024-11-30 23:55:48 +0100

    add treesitter context

--- nvim/.config/nvim/lua/mtyszkiewicz/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/mtyszkiewicz/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
     {
+        "nvim-treesitter/nvim-treesitter-context"
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
         dependencies = {
             "nvim-treesitter/nvim-treesitter-context",
         },
