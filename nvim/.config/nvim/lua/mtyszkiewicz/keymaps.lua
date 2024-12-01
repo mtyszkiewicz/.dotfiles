591f31c060ae43b4c4bd17c61ea493bc2eb97d19
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2024-12-02 00:46:26 +0100

    add useful kepmaps

--- nvim/.config/nvim/lua/mtyszkiewicz/keymaps.lua
+++ nvim/.config/nvim/lua/mtyszkiewicz/keymaps.lua
@@ -2,6 +2,16 @@
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<", "<gv")
 
+-- Easier jumping to start/end of the line
+vim.keymap.set("n", "E", "$")
+vim.keymap.set("n", "B", "^")
+
+-- Keep paste buffer after overwrite
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Keep paste buffer after delete
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
 -- Yank to system clipboard using Space+y
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
