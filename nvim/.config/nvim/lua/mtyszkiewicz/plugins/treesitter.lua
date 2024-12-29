a74a9ec652359e9efa89c36a21a69f5717ff4342
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2024-12-29 22:01:26 +0100

    fix treesiter

--- nvim/.config/nvim/lua/mtyszkiewicz/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/mtyszkiewicz/plugins/treesitter.lua
@@ -4,28 +4,33 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         -- dependencies = { "nvim-treesitter/nvim-treesitter-context" },
-        opts = {
-            highlight = { enable = true },
-            indent = { enable = true },
-            auto_install = true,
-            ensure_installed = {
-                "bash",
-                "diff",
-                "dockerfile",
-                "java",
-                "json",
-                "lua",
-                "markdown",
-                "python",
-                "query",
-                "regex",
-                "sql",
-                "terraform",
-                "toml",
-                "vim",
-                "vimdoc",
-                "yaml",
-            }
-        }
+        config = function()
+            require("nvim-treesitter.configs").setup(
+                {
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                    auto_install = true,
+                    ensure_installed = {
+                        "bash",
+                        "c",
+                        "diff",
+                        "dockerfile",
+                        "java",
+                        "json",
+                        "lua",
+                        "markdown",
+                        "python",
+                        "query",
+                        "regex",
+                        "sql",
+                        "terraform",
+                        "toml",
+                        "vim",
+                        "vimdoc",
+                        "yaml",
+                    }
+                }
+            )
+        end
     }
 }
