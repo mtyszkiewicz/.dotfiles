7b9e9ec8d5259f4ff8c3769a9baa5fa1d055232a
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2025-04-06 20:15:30 +0200

    replace poetry with uv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -49,7 +49,6 @@ alias wr="wormhole receive"
 alias ws="wormhole send"
 alias nv="nvim ."
 alias c="clear"
-alias activate_poetry="source \"\$(poetry env list --full-path | grep Activated | cut -d' ' -f1 )/bin/activate.fish\""
 
 set fish_greeting  # disable fish greeting
 
