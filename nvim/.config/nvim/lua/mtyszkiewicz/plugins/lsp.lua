d39eaf0a92bf9a2c9856a0a1382a2b13a73217ae
Marcin Tyszkiewicz <56219944+mtyszkiewicz@users.noreply.github.com>
2026-05-09 16:01:19 +0200

    audit neovim setup

--- nvim/.config/nvim/lua/mtyszkiewicz/plugins/lsp.lua
+++ nvim/.config/nvim/lua/mtyszkiewicz/plugins/lsp.lua
@@ -2,62 +2,64 @@ return {
     {
         "neovim/nvim-lspconfig",
         dependencies = {
-            "folke/neodev.nvim",
+            "folke/lazydev.nvim",   -- replaces neodev
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
-            "nvimtools/none-ls.nvim",
-            "jay-babu/mason-null-ls.nvim",
             "hrsh7th/nvim-cmp",
             "hrsh7th/cmp-nvim-lsp",
         },
         config = function()
             local cmp_nvim_lsp = require("cmp_nvim_lsp")
-            local lspconfig = require("lspconfig")
             local mason = require("mason")
             local mason_lspconfig = require("mason-lspconfig")
-            -- local mason_null_ls = require("mason-null-ls")
-            local neodev = require("neodev")
-            -- local null_ls = require("null-ls")
             local telescope_builtin = require("telescope.builtin")
 
-            vim.api.nvim_create_autocmd(
-                "LspAttach",
-                {
-                    callback = function(event)
-                        local opts = { buffer = event.bufnr, remap = true }
+            -- Wire up cmp capabilities globally for all servers
+            vim.lsp.config("*", {
+                capabilities = cmp_nvim_lsp.default_capabilities(),
+            })
 
-                        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-                        vim.keymap.set("n", "<leader>vrr", telescope_builtin.lsp_references, opts)
-                        vim.keymap.set("n", "<leader>vws", telescope_builtin.lsp_dynamic_workspace_symbols, opts)
-                        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-                        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+            -- LSP keymaps (unchanged, this part was fine)
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(event)
+                    local opts = { buffer = event.bufnr, remap = true }
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "<leader>vrr", telescope_builtin.lsp_references, opts)
+                    vim.keymap.set("n", "<leader>vws", telescope_builtin.lsp_dynamic_workspace_symbols, opts)
+                    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+                end,
+            })
+
+            -- Format on save, skip C files (unchanged)
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                callback = function(event)
+                    if vim.bo.filetype ~= "c" then
+                        vim.lsp.buf.format({ async = false, bufnr = event.bufnr })
                     end
-                }
-            )
+                end,
+            })
 
-            -- Format code before file write
-            vim.api.nvim_create_autocmd(
-                "BufWritePre",
-                {
-                    callback = function(event)
-                        -- Check the file type before formatting
-                        local filetype = vim.bo.filetype
-                        if filetype ~= "c" then
-                            vim.lsp.buf.format({
-                                async = false,
-                                bufnr = event.bufnr,
-                            })
-                        end
-                    end
-                }
-            )
+            -- Pyright config (was silently broken before — assigned table instead of calling setup)
+            vim.lsp.config("pyright", {
+                settings = {
+                    pyright = {
+                        disableOrganizeImports = true,
+                    },
+                    python = {
+                        analysis = {
+                            ignore = { "*" },
+                            typeCheckingMode = "off",
+                        },
+                    },
+                },
+            })
 
-            neodev.setup()
             mason.setup()
+
             mason_lspconfig.setup({
                 ensure_installed = {
                     "bashls",
@@ -65,74 +67,29 @@ return {
                     "jsonls",
                     "lua_ls",
                     "pyright",
-                    "ruff",
                     "yamlls",
                     "sqlls",
                     "clangd",
                 },
-                handlers = {
-                    -- default handler
-                    function(server)
-                        lspconfig[server].setup({
-                            capabilities = cmp_nvim_lsp.default_capabilities(),
-                        })
-                    end,
-                },
             })
 
-            lspconfig.ruff.setup({
-                init_options = {
-                    lineLength = 100,
-
-                    select = {
-                        "E",  -- pycodestyle errors
-                        "W",  -- pycodestyle warnings
-                        "F",  -- pyflakes
-                        "I",  -- isort
-                        "UP", -- pyupgrade
-                    },
-
-                    ignore = {
-                        "E501", -- Line too long (handled by line length setting)
-                    },
-                }
-            })
-
-            -- Better intellisense than ruff
-            lspconfig.pyright = {
-                settings = {
-                    pyright = {
-                        disableOrganizeImports = true, -- Using Ruff
-                    },
-                    python = {
-                        analysis = {
-                            ignore = { '*' },         -- Using Ruff
-                            typeCheckingMode = 'off', -- Using mypy
-                        },
-                    },
-                },
+            -- Enable all servers (replaces the handlers default function)
+            local servers = {
+                "bashls", "gopls", "jsonls", "lua_ls",
+                "pyright", "yamlls", "sqlls", "clangd",
             }
-
-            lspconfig.clangd.setup({
-
-            })
-
-            -- mason_null_ls.setup({
-            --     automatic_installation = true,
-            --     ensure_installed = {
-            --         "autoflake",
-            --         "black",
-            --         "isort",
-            --     },
-            -- })
-            --
-            -- null_ls.setup({
-            --     sources = {
-            --         null_ls.builtins.formatting.autoflake,
-            --         null_ls.builtins.formatting.black,
-            --         null_ls.builtins.formatting.isort,
-            --     }
-            -- })
+            for _, server in ipairs(servers) do
+                vim.lsp.enable(server)
+            end
         end,
+    },
+    {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
     },
 }
